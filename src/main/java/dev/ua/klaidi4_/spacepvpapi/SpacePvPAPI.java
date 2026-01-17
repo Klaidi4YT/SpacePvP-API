32724c5e4dc566c4430927a090af4c91d33c25f7
Klaidi4YT <maksym.ostapenko07@gmail.com>
2026-01-17 15:16:37 +0200

    1.0

--- src/main/java/dev/ua/klaidi4_/spacepvpapi/SpacePvPAPI.java
+++ src/main/java/dev/ua/klaidi4_/spacepvpapi/SpacePvPAPI.java
@@ -1,8 +1,26 @@
 package dev.ua.klaidi4_.spacepvpapi;
 
-import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import java.util.UUID;
 
 public interface SpacePvPAPI {
-    int getWins(Player player);
+
+    /**
+     * Gets the total number of wins for a specified player.
+     * If the player is not found, returns 0.
+     */
+    int getWins(@NotNull UUID playerUUID);
+
+    /**
+     * Gets the total number of losses for a specified player.
+     * If the player is not found, returns 0.
+     */
+    int getLosses(@NotNull UUID playerUUID);
+
+    /**
+     * Gets the total amount of points for a specified player.
+     * If the player is not found, returns 0.
+     */
+    int getPoints(@NotNull UUID playerUUID);
 
 }
