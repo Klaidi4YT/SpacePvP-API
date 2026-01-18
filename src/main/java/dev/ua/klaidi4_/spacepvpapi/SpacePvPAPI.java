10999881d23492c6c3059a4cbaa8a317cc40e615
_Klaidi4_ <163753798+Klaidi4YT@users.noreply.github.com>
2026-01-18 07:30:07 +0200

    1.0

--- src/main/java/dev/ua/klaidi4_/spacepvpapi/SpacePvPAPI.java
+++ src/main/java/dev/ua/klaidi4_/spacepvpapi/SpacePvPAPI.java
@@ -4,23 +4,30 @@ import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
 public interface SpacePvPAPI {
-
     /**
      * Gets the total number of wins for a specified player.
      * If the player is not found, returns 0.
      */
     int getWins(@NotNull UUID playerUUID);
-
     /**
      * Gets the total number of losses for a specified player.
      * If the player is not found, returns 0.
      */
     int getLosses(@NotNull UUID playerUUID);
-
     /**
      * Gets the total amount of points for a specified player.
      * If the player is not found, returns 0.
      */
     int getPoints(@NotNull UUID playerUUID);
-
+    /**
+     * Gets the current win streak for a specified player.
+     * If the player is not found, returns 0.
+     */
+    int getCurrentWinStreak(@NotNull UUID playerUUID);
+    /**
+     * Gets the Kill/Death ratio for a specified player.
+     * Usually calculated as kills divided by deaths.
+     * If the player is not found, returns 0.0.
+     */
+    double getKDRadio(@NotNull UUID playerUUID);
 }
